02eaa8cfb029f73f5b7ac6785b11d5a708166c63
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-21 16:00:18 +0400

    add project asp.net

---
+++ GraphPlot/GraphPlot/Controllers/PlotController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GraphPlot.Controllers
+{
+    public class PlotController : ApiController
+    {
+        // GET: api/Plot
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // PUT: api/Plot/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+    }   
+}
