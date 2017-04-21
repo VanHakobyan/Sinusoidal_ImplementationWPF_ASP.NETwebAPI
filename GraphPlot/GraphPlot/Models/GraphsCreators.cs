cabc655993443d5fe4de754748472ed324bf2fbb
vanhakobyan <vanhakobyan1996@gmail.com>
2017-04-21 16:07:10 +0400

    add models :two:

---
+++ GraphPlot/GraphPlot/Models/GraphsCreators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphPlot.Models
+{
+    public class GraphsCreator
+    {
+        private List<Point> xyList;
+
+
+        public List<Point> GetXY()
+        {
+            xyList = new List<Point>();
+
+            for (int i = 0; i < 70; i++)
+            {
+                var x = i / 5.0;
+                var y = Math.Sin(x);
+                xyList.Add(new Point() { X = x, Y = y });
+            }
+
+
+            return xyList;
+        }
+
+
+
+    }
---
+++ GraphPlot/GraphPlot/Models/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GraphPlot.Models
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+    }
+}
